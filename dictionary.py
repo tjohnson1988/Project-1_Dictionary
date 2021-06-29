a87d4b17666b9260f17454eac6e92ca49e90807b
tjohnson1988 <85202888+tjohnson1988@users.noreply.github.com>
2021-06-29 08:06:25 -0400

    get_close_matches update

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,6 @@
 import json #imports data file 
-#import difflib #imports standard python module - using for sequence matcher
+import difflib #imports standard python module - using for sequence matcher
+from difflib import get_close_matches 
 
 data = json.load(open("/Users/trippjohnson/Udemy/Project 1_Dictionary/data.json"))
 
@@ -7,6 +8,8 @@ def lookup(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
     else:
         return "The word doesn't exit. Please double check it."
 
